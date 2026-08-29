58732e8a1912b295492c60c4d21daa9db88c654c
Ricardo <cesar15bernardo@gmail.com>
2026-08-29 14:57:13 +0100

    Fix: Middleware  Proxy v2 conflits with Path

--- apps/api-gateway/main.ts
+++ apps/api-gateway/main.ts
@@ -15,21 +15,16 @@ app.use(
   createProxyMiddleware({
     target: process.env.EVENT_SERVICE_URL || "http://localhost:3001",
     changeOrigin: true,
-    pathRewrite: {
-      "^/api/events": "/api/events",
-    },
+    pathRewrite: (path, req) => "/api/events" + path,
   }),
 );
 
-// Proxy para o User Service (Porta 3002)
 app.use(
   "/api/users",
   createProxyMiddleware({
     target: process.env.USER_SERVICE_URL || "http://localhost:3002",
     changeOrigin: true,
-    pathRewrite: {
-      "^/api/users": "/api/users",
-    },
+    pathRewrite: (path, req) => "/api/users" + path,
   }),
 );
 
